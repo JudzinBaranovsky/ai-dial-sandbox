72edfbc32b01303f8e43e6f97583c859374fd455
Yauhen Baranouski <yauhen_baranouski@epam.com>
2025-07-05 10:41:33 +0300

    Add instructions on how to use Qodo Merge agent with Azure DevOps. Ensure a TCP dump folder for monitoring Qodo Merge is auto-created. Add support for Qodo Merge "ask" command for free-form talk-to-you-code.

--- qodo.ps1
+++ qodo.ps1
@@ -1,6 +1,7 @@
 param (
     [string]$prAction = "not_defined",
-    [string]$prUrl = "not_defined"
+    [string]$prUrl = "not_defined",
+    [string]$prompt = "not_defined"
 )
 
 $tempEnvFile = "qodo-merge-agent/temp.env"
@@ -10,6 +11,15 @@ if (Test-Path $tempEnvFile) {
 
 Add-Content -Path $tempEnvFile -Value "GITHUB_PR_URL=$prUrl"
 Add-Content -Path $tempEnvFile -Value "PR_ACTION=$prAction"
+
+$tcpDumpFolder = "qodo-merge-agent/tcpdump"
+if (-not (Test-Path $tcpDumpFolder)) {
+    New-Item -ItemType Directory -Path $tcpDumpFolder | Out-Null
+}
+
+if ($prAction -eq "ask") {
+    Add-Content -Path $tempEnvFile -Value "ASK_PROMPT=$prompt"
+}
 
 $dockerComposeFile = "qodo-merge-agent/docker-compose.yml"
 docker compose --env-file $tempEnvFile -f $dockerComposeFile up --build --remove-orphans
